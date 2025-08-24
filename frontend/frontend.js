702b494035db8dd81d80af825a9b74741a665b8d
OakLeaf007 <jjjbbbb736@gmail.com>
2025-08-24 12:56:03 +0530

    Updated 2.0

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -31,3 +31,25 @@ form.addEventListener("submit", async (e) => {
     }
 });
 
+
+// Google login 
+function handleCredentialResponse(response) {
+    fetch(`/api/auth/google-login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ token: response.credential })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.user) {
+            document.getElementById("message").textContent = data.message || "";
+            localStorage.setItem("userEmail", data.user.email); // store email
+            localStorage.setItem("usrname", data.user.name);
+            localStorage.setItem("loggedIn", "true");
+            window.location.href = "/logged.html";
+        } else {
+            document.getElementById("message").textContent = data.message || data.error;
+        }
+    })
+    .catch(err => console.error("Google login error:", err));
+}
